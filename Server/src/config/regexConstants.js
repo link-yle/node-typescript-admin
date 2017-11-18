dd1f190166b388115611dc543d0c2535cd4166cf
link-yle <kylelin.dev@gmail.com>
2017-11-18 06:58:37 +0200

    Allow special characters in regex password

--- Server/src/config/regexConstants.js
+++ Server/src/config/regexConstants.js
@@ -1,4 +1,4 @@
 const emailRegex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!$%@#£€*?&]{8,}$/;
 
 module.exports = {emailRegex, passwordRegex}
