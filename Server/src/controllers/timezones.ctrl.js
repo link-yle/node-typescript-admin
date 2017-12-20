e670770226702a5afd587d9ec8792d70b33a65f5
link-yle <kylelin.dev@gmail.com>
2017-12-20 11:46:48 +0200

    Clean up ctrls

--- Server/src/controllers/timezones.ctrl.js
+++ Server/src/controllers/timezones.ctrl.js
@@ -1,23 +1,18 @@
 const utility = require('../helpers/utility.js')
 const db = require('../database/db.ctrl')
 
-async function removeTimeZone(req, res, next) {
-    
-    const update = await db.removeTimeZone(req.params.id, req.params.timeZoneId).catch(err=>next(err))
-    if (!update) return next(utility.resourceNotFound('timezone'))
-    return res.status(200).json(update)
+function removeTimeZone(req, res, next) {
+    return db.removeTimeZone(req.params.id, req.params.timeZoneId)
+    .then(update=> update ?  res.status(200).json(update) : next(utility.resourceNotFound('timezone')))
+    .catch(err=>next(err))
 }
 
-async function addTimeZone(req, res, next) {
-    const user = await db.addTimeZone(req.params.id, req.body).catch(err=>next(err))
-    .catch(err => utility.badRequest(res, 'to add timeZone'))
-    return res.status(200).json(user)
+function addTimeZone(req, res, next) {
+    return db.addTimeZone(req.params.id, req.body).catch(err=>next(err)).then(user=>res.status(200).json(user)) 
 }
 
-async function updateTimeZone(req, res, next) {
-    const user = await db.updateTimeZone(req.params.id, req.params.timeZoneId, req.body).catch(err=>next(err))
-    .catch(err => utility.badRequest(res, 'to remove timeZone'))
-    return res.status(200).json(user)
+function updateTimeZone(req, res, next) {
+    return db.updateTimeZone(req.params.id, req.params.timeZoneId, req.body).catch(err=>next(err)).then(user=>res.status(200).json(user))
 }
 
 module.exports = { updateTimeZone, addTimeZone, removeTimeZone }
--- Server/src/controllers/users.ctrl.js
+++ Server/src/controllers/users.ctrl.js
@@ -4,20 +4,19 @@ const usersModel = require('../models/users.model')
 const { getToken } = require('../core/authentication')
 const ROLES = require('../config/rolesConstants')
 const db = require('../database/db.ctrl')
+process.on('unhandledRejection', up => { throw up })
 
-async function signup(req, res) {
-    const added = await db.addNewUser(req.body, ROLES.regular).catch(err => {
-        if (err.code === 11000 && err.index === 0) return res.status(409).json('Email already exists')
-        else return utility.badRequest(res, 'signup you')
-    })
-    delete added.password
-    return res.status(200).json(added)
+function signup(req, res, next) {
+    return db.addNewUser(req.body, ROLES.regular).then(added => {
+        delete added.password
+        return res.status(200).json(added)
+    }).catch(e => next(e))
 }
 
 
 
 function login(req, res) {
-    usersModel.findOne({ email: req.body.email }, (err, user) => {
+    return usersModel.findOne({ email: req.body.email }, (err, user) => {
         if (err) throw err;
         if (!user) return utility.notFound(res, 'user')
         user.comparePassword(req.body.password, (err, isMatch) => {
@@ -31,40 +30,38 @@ function login(req, res) {
 }
 
 
-async function findUserAndUpdateInfo(req, res) {
-    const user = await db.updateUserInfo(req.params.id, req.body.email, req.body.name)
-        .catch(err => utility.badRequest(res, 'save updated info'))
-    return res.status(200).json(user)
-}
-
-async function findUserAndUpdateRole(req, res) {
-    const user = db.updateRole(req.params.id, req.body.role).catch(err => utility.badRequest(res, 'save updated role'))
-    if (!user) return utility.notFound(res, 'user')
-    return res.status(200).json(user)
-}
-
-
-async function getUserDetails(req, res) {
-    const user = await db.getUserDetails(req.params.id).catch(err => utility.badRequest(res, err))
-    return res.status(200).json(user)
+function findUserAndUpdateInfo(req, res, next) {
+    return db.updateUserInfo(req.params.id, req.body.email, req.body.name).then(user => res.status(200).json(user))
+        .catch(err => next(err))
 
 }
 
-async function getUsers(req, res, next) {
-    
-    let users, count
+function findUserAndUpdateRole(req, res, next) {
+    return db.updateRole(req.params.id, req.body.role).then(user => {
+        if (!user) return utility.resourceNotFound('user')
+        return res.status(200).json(user)
+    }).catch(err => next(err))
+
+}
+
+
+function getUserDetails(req, res, next) {
+    return db.getUserDetails(req.params.id).then(x=>res.status(200).json(x)).catch(err => next(err))
+}
+
+function getUsers(req, res, next) {
+    let promise
+    const query = { limit: 10, skip: parseInt(req.query.skip) }
     if (req.decoded.role === ROLES.manager) {
-        [users, count] = await Promise.all([db.getRegularUsers({ limit: 10, skip: req.query.skip }), db.getRegularUsers()]).catch(err => next(err))
+        promise = Promise.all([db.getRegularUsers(query), db.getRegularUsersCount()])
     } else {
-        [users, count] = await Promise.all([db.getAllUsers({ limit: 10, skip: parseInt(req.query.skip) }), db.getAllUsersCount()])
-        .catch(err => {  global.log.error(err) } )
+        promise = Promise.all([db.getAllUsers(query), db.getAllUsersCount()])
     }
-    return res.status(200).json({users, count})
+    return promise.then(([users, count]) => res.status(200).json({ users, count })).catch(err => next(err))
 }
 
-async function removeUser(req, res) {
-    await db.removeUser(req.params.id).catch(err => res.status(400).json(err))
-    return res.status(200).json("Ok")
+function removeUser(req, res, next) {
+    return db.removeUser(req.params.id).then(()=>res.status(200).json("Ok")).catch(err => next(err))
 }
 
 
