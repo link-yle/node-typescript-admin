d68aba84eac2c148b0cce14d2f3b6715e1841fc3
link-yle <kylelin.dev@gmail.com>
2017-12-31 05:05:47 +0200

    Add fetching user data after refresh

--- Client/src/app/routes/edit-role/edit-role.component.ts
+++ Client/src/app/routes/edit-role/edit-role.component.ts
@@ -1,6 +1,6 @@
 import { DataService } from '../../shared/services/data.service';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { SnackBarService } from '../../shared/services/snackbar.service';
 import { SelectedUserService } from '../../shared/services/selectedUser.service';
 
@@ -10,22 +10,34 @@ import { SelectedUserService } from '../../shared/services/selectedUser.service'
 })
 export class EditRoleComponent implements OnInit {
     userRole: string
+    userId: string
+    userName: string
     constructor(
         private dataService: DataService,
         private router: Router,
         private selectedUserService: SelectedUserService,
-        private sb: SnackBarService
-    ) {}
+        private sb: SnackBarService,
+        private route: ActivatedRoute,
+    ) { }
 
     ngOnInit() {
-        this.userRole = this.selectedUserService.get().role
+        this.selectedUserService.getUserWithProbableDataFetch(this.route.params).subscribe(
+            user => {
+                this.userRole = user.role
+                this.userId = user._id
+                this.userName = user.name
+            }
+        )
+       
     }
+
     onAssignClick() {
-        this.dataService.assignRole(this.selectedUserService.get()._id, { role: this.userRole }).subscribe(
+        this.dataService.assignRole(this.userId, { role: this.userRole }).subscribe(
             data => {
                 this.sb.emitSuccessSnackBar()
                 this.router.navigate(['/users'])
-            }
+            },
+            error => this.sb.emitErrorSnackBar(error)
         )
     }
 
--- Client/src/app/shared/services/selectedUser.service.ts
+++ Client/src/app/shared/services/selectedUser.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
+import { ActivatedRoute } from '@angular/router/src/router_state';
+import { Observable } from 'rxjs/Observable';
+import { DataService } from 'app/shared/services/data.service';
 
 @Injectable()
 export class SelectedUserService {
 
     private selectedUser: User
 
+    constructor(
+        private dataService: DataService
+    ) { }
     public set(user: User): void {
         this.selectedUser = user;
     }
 
     public get(): User {
         return this.selectedUser
+    }
+
+    public getUserWithProbableDataFetch(params) {
+        if (this.selectedUser) return Observable.of(this.selectedUser)
+        else return params.first().switchMap(data => this.dataService.getUserDetails(data.id).first().do(user=> this.selectedUser = user))
     }
 
 
